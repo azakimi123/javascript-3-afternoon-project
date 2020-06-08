0077833ab7ca8de990b20023ae347f0ae18feff6
azakimi123 <aaron.zakimi@gmail.com>
2020-06-08 16:37:54 -0600

    problem 1

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,10 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+// let evenNumbers = mixedNumbers.filter(even => even % 2 === 0)
+let evenNumbers = mixedNumbers.filter(function(even){
+  return even % 2 === 0;
+})
 
 
 
